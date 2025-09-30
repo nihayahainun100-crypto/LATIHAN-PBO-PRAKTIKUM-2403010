4658b765ddebab4f9fed836e5a0e6cbdb585dbcd
nihayahainun100-crypto <nihayahainun100@gmail.com>
2025-09-30 14:56:36 +0700

    praktikum4 (2403010)

---
+++ praktikum4/inheritance/multiple/mainMultipleInheritanceTesh.java
@@ -0,0 +1,8 @@
+package multiple;
+// Demo
+public class mainMultipleInheritanceTesh {
+    public static void main(String[] args) {
+        MahasiswaKerja john = new MahasiswaKerja("John");
+        john.jadwalHarian();
+    }
+}
